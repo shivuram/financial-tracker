c56b22c9f0374185c9cbff8027682f0eadc58c47
Sivasubramanian Ramanathan <Sivasubramanian.ramnath@gmail.com>
2025-08-30 08:37:14 +0530

    Update Add Jobs Page with forms

--- src/pages/Job-Portal/Add-jobs.tsx
+++ src/pages/Job-Portal/Add-jobs.tsx
@@ -1,5 +1,53 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import type { Job } from "../../types/transaction";
+
 const AddJobs = () => {
-  return <div className="tracker-container">AddJobs</div>;
+  const navigate = useNavigate();
+  const [jobs, setJobs] = useState<Job[]>([]);
+  const [totalCount, setTotalCount] = useState<number>(0);
+
+  const addJobsHandler = async () => {
+    const newJob = {
+      title: "React Engineer",
+      location: "Chennai",
+      company: "SCB",
+      salary: "25 LPA",
+    };
+    await fetch("http://localhost:5000/jobs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newJob),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setJobs(data.data);
+        setTotalCount(data.meta.totalCount);
+      });
+  };
+
+  return (
+    <>
+      <div className="tracker-container">
+        <div className="add-jobs-header">
+          <button onClick={() => navigate("/job-portal")}>Back</button>
+        </div>
+        <div className="form">
+          <label htmlFor="Title">Title</label>
+          <input type="text" />
+          <label htmlFor="Title">Company</label>
+          <input type="text" />
+          <label htmlFor="Title">Location</label>
+          <input type="text" />
+          <label htmlFor="Title">Salary (LPA)</label>
+          <input type="number" />
+          <button onClick={addJobsHandler}>Submit</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AddJobs;
